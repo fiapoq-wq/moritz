a6a285155471729be36ccd3843c5fccb80d423e0
fiapoq-wq <fiapoq@gmail.com>
2026-07-29 13:54:21 -0300

    Add files via upload

---
+++ public/firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "COLE_AQUI",
+  authDomain: "SEU-PROJETO.firebaseapp.com",
+  projectId: "SEU-PROJETO",
+  storageBucket: "SEU-PROJETO.firebasestorage.app",
+  messagingSenderId: "COLE_AQUI",
+  appId: "COLE_AQUI"
+};
